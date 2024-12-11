62c732d418420a2a277c45209040e2c6d57f3dfc
MVRK0 <elikemfenuksu@gmail.com>
2024-12-11 12:11:41 +0000

    initial commit

--- app/src/main/java/org/example/Book.java
+++ app/src/main/java/org/example/Book.java
@@ -1,5 +1,57 @@
-public class Book {
+package org.example;
 
-    // This is your working class.
-    
+public class Book {
+    private String title;
+    private String author;
+    private int yearPublished;
+    private String bookISBN;
+
+    public Book(String title, String author, int yearPublished, String bookISBN) {
+        this.title = title;
+        this.author = author;
+        this.yearPublished = yearPublished;
+        this.bookISBN = bookISBN;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getYearPublished() {
+        return yearPublished;
+    }
+
+    public void setYearPublished(int yearPublished) {
+        this.yearPublished = yearPublished;
+    }
+
+    public String getBookISBN() {
+        return bookISBN;
+    }
+
+    public void setBookISBN(String bookISBN) {
+        this.bookISBN = bookISBN;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", yearPublished=" + yearPublished +
+                '}';
+    }
+
 }
